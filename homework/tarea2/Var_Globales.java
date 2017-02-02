a617f5367dc9de2d90785d6c6d4f275b56048aa0
everardo <ieimethod1@gmail.com>
2017-02-02 17:30:33 -0600

    complemento tarea2

---
+++ homework/tarea2/Var_Globales.java
@@ -0,0 +1,25 @@
+public class Var_Globales{
+	
+	public static byte var_byte;	
+	public static short var_short;
+	public static int var_int;
+	public static long var_long;
+	public static double var_double;
+    public static char var_char;
+	public static String var_string;
+	public static boolean var_boolean;
+public static void main(String[]args){
+	
+	System.out.println("Valor de byte "+var_byte);
+	System.out.println("Valor de short "+var_short);
+	System.out.println("Valor de int "+var_int);
+	System.out.println("Valor de long "+var_long);
+	System.out.println("Valor de double "+var_double);
+	System.out.println("Valore de char "+var_char);
+	System.out.println("Valor de String "+var_string);
+	System.out.println("Valor de boolean "+var_boolean);
+
+}
+
+
+}
